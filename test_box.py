2972844f5fc3a18bee33f109b67bc2cdf290b38c
Huimin <784453793@qq.com>
2017-10-31 21:03:19 -0400

    pass test_box

--- test_box.py
+++ test_box.py
@@ -5,7 +5,7 @@ import simulator_config
 
 env = Simulator(simulator_config)
 map = Map()
-map.get_map_from_geom2d(env, kp=np.array([[-100, 100], [-100, -100], [100, -100], [100, 100]]), n_pts=180)
+map.get_map_from_geom2d(env, kp=np.array([[-100, 100], [-100, -100], [100, -100], [100, 100]]))
 
 robot = Agent(env, kp=np.array([[-2, 0], [2, 0]]), color=(1, 0, 0, 0.5), v_max=5)
 robot.reset(init_state=np.array([0, 40, 0]))
